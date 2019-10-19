921b4cbd5472be918fda27888d6e7dcdfe58e275
John Chan <johnchancontact@gmail.com>
2019-10-19 13:29:11 -0400

    implemented voting system

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const userRouter = require("./user/user-router");
 const articlesRouter = require("./articles/articles-router");
 const commentsRouter = require("./comments/comments-router");
 const tagsRouter = require("./tags/tags-router");
+const votesRouter = require("./votes/votes-router");
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.use("/api/user", userRouter);
 app.use("/api/articles", articlesRouter);
 app.use("/api/comments", commentsRouter);
 app.use("/api/tags", tagsRouter);
+app.use("/api/votes", votesRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
