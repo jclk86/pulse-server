89c8613f635144a3665a254ffaccdd2006d07706
John Chan <johnchancontact@gmail.com>
2019-10-20 18:00:23 -0400

    voting implemented

--- src/votes/votes-router.js
+++ src/votes/votes-router.js
@@ -9,9 +9,19 @@ votesRouter
   .route("/:article_id")
   .get((req, res, next) => {
     VotesService.getTotalVotes(req.app.get("db"), req.params.article_id).then(
-      votes => res.json(votes)
+      votes => res.json(votes.map(vote => VotesService.serializeVotes(vote))) //filter it here?
     );
   })
+  .delete(requireAuth, (req, res, next) => {
+    const { article_id } = req.params.article_id;
+    const user_id = req.user.id;
+    VotesService.deleteVote(req.app.get("db"), article_id, user_id)
+      .then(numRowsAffected => {
+        res.status(204).end();
+      })
+      .catch(next);
+  })
+
   .patch(requireAuth, bodyParser, (req, res, next) => {
     const { voted } = req.body;
     const { article_id } = req.params;
--- src/votes/votes-service.js
+++ src/votes/votes-service.js
@@ -2,10 +2,41 @@ const VotesService = {
   getTotalVotes(db, article_id) {
     return db
       .from("travelist_votes as votes")
-      .select("article_id", db.raw(`count(voted) as num_of_votes`))
+      .select(
+        "article_id",
+        "id",
+        "user_id",
+        "voted",
+        db.raw(`count(voted) as num_of_votes`)
+      )
       .where("votes.article_id", article_id)
       .andWhere("votes.voted", true)
-      .groupBy("article_id", "voted");
+      .groupBy("article_id", "voted", "id", "user_id");
+  },
+  getByIds(db, article_id, user_id) {
+    return db
+      .from("travelist_votes")
+      .select("*")
+      .where({ article_id })
+      .andWhere({ user_id })
+      .first();
+  },
+  addVote(db, newVote) {
+    return db
+      .insert(newVote)
+      .into("travelist_votes")
+      .returning("*")
+      .then(([votes]) => votes)
+      .then(votes =>
+        VotesService.getByIds(db, votes.article_id, votes.user_id)
+      );
+  },
+  deleteVote(db, article_id, user_id) {
+    return db
+      .from("travelist_votes")
+      .where({ article_id })
+      .andWhere({ user_id })
+      .del();
   },
   updateNumOfVotes(db, article_id, user_id, newVoteCount) {
     return db
@@ -13,6 +44,16 @@ const VotesService = {
       .where({ article_id })
       .andWhere({ user_id })
       .update(newVoteCount);
+  },
+
+  serializeVotes(votes) {
+    return {
+      id: votes.id,
+      article_id: votes.article_id,
+      voted: votes.voted,
+      num_of_votes: votes.num_of_votes,
+      user_id: votes.user_id
+    };
   }
 };
 
