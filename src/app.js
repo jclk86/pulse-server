552f2dc28417a5532cd95c8ef4c3fc60311376e1
John Chan <johnchancontact@gmail.com>
2019-11-15 13:16:52 -0500

    fixing cors issue

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.options("/api/auth", cors());
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
