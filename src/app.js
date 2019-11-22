c54080198feccd04f03a0c875eb90e23aac2bde3
John Chan <johnchancontact@gmail.com>
2019-11-22 11:45:13 -0500

    added public route for static files

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.use(express.static("/public"));
+app.use("/public", express.static("public"));
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
