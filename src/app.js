c6f03eb30457905d769eee41c0ca447c63945154
John Chan <johnchancontact@gmail.com>
2019-11-22 10:52:48 -0500

    added static public url to app

--- src/app.js
+++ src/app.js
@@ -18,7 +18,8 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
-app.use(express.static("public"));
+
+app.use(express.static("/public"));
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
