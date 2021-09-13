3698f0b7b7895dbc91fc377c7f26856b5ec49ea5
John Chan <johnchancontact@gmail.com>
2021-09-13 18:39:59 -0400

    FIX: cors

--- src/app.js
+++ src/app.js
@@ -18,11 +18,7 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 
-app.use(
-  cors({
-    origin: "https://arcane-refuge-94724.herokuapp.com",
-  })
-);
+app.use(cors());
 
 app.use("/public", express.static("public"));
 
