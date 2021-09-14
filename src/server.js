312d43a5f1eda5592d373b6788c63cfc71742b74
John Chan <johnchancontact@gmail.com>
2021-09-14 00:46:51 -0400

    FIX: added ssl config to knex

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ const knex = require("knex");
 
 const db = knex({
   client: "pg",
-  connection: DATABASE_URL
+  connection: DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 app.set("db", db);
